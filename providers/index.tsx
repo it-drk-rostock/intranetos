c0436ad921b2fc92d709e9abddc5950b415d42dd
MarcellusStack <Marcellusstack@gmail.com>
2024-10-17 13:48:49 +0200

    Refactor layout component to use Mantine UI library and import required dependencies

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,21 +1,32 @@
+"use client";
 import { getQueryClient } from "@lib/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
 import { MantineProvider } from "@providers/mantine-provider";
+import { SessionProvider } from "next-auth/react";
 
+import { Session } from "next-auth";
 /* const DirectusPageView = dynamic(() => import("@lib/directus/directus-page-view"), {
   ssr: false,
 }); */
 
-export const Providers = ({ children }: { children: React.ReactNode }) => {
+export const Providers = async ({
+  session,
+  children,
+}: {
+  session: Session | null;
+  children: React.ReactNode;
+}) => {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider>
-        {/* <DirectusPageView /> */}
-        {children}
-      </MantineProvider>
-    </QueryClientProvider>
+    <SessionProvider session={session} refetchInterval={30}>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider>
+          {/* <DirectusPageView /> */}
+          {children}
+        </MantineProvider>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 };
