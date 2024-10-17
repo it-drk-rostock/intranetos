b505be781ea313218436166e77c210535bc3b434
MarcellusStack <Marcellusstack@gmail.com>
2024-10-17 09:41:02 +0200

    Refactor AuthLayout component to use a smaller maximum width

--- components/layouts/auth-layout.tsx
+++ components/layouts/auth-layout.tsx
@@ -8,7 +8,7 @@ export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       <Flex className="h-full w-full" w="100%" h="100%">
         <Stack w="50%" p="md" h="100%" justify="space-between">
           <Navbar />
-          <Stack maw="450px" miw="450px" mx="auto">
+          <Stack maw="400px" miw="400px" mx="auto">
             {children}
           </Stack>
           <Box />
