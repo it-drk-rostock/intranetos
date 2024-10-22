845288e44d76a1af8cbb037dca7790127d3b1a21
MarcellusStack <MarcellusStack@gmail.com>
2024-10-22 13:16:55 +0200

    Refactor DirectusPageView to track pageviews and store them in the page_analytics collection

--- lib/directus/directus-page-view.tsx
+++ lib/directus/directus-page-view.tsx
@@ -1,26 +1,39 @@
-/* "use client";
+"use client";
 
+import { createItem } from "@directus/sdk";
+import { useSession } from "next-auth/react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
+import { client } from "./clients";
 
 const DirectusPageView = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-   const directus = useDirectus(); 
-   const isDevelopment = process.env.NODE_ENV === "development";
-const isLocalhost = window.location.hostname === "localhost";
+  const session = useSession();
+  const analyticsToken = process.env.NEXT_PUBLIC_ANALYTICS_TOKEN;
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const isLocalhost = window.location.hostname === "localhost";
+
   useEffect(() => {
-Track pageviews
-    if (pathname && directus && !isDevelopment && !isLocalhost) {
+    if (pathname && !isDevelopment && !isLocalhost) {
       let url = window.origin + pathname;
       if (searchParams.toString()) {
         url = url + `?${searchParams.toString()}`;
       }
-      directus.capture(url);
+
+      const token = session.data?.token || analyticsToken;
+
+      if (token) {
+        client(token).request(
+          createItem("page_analytics", {
+            path: url,
+          })
+        );
+      }
     }
-  }, [pathname, searchParams, directus]);
+  }, [pathname, searchParams, session, analyticsToken]);
 
   return null;
 };
 
-export default DirectusPageView; */
+export default DirectusPageView;
